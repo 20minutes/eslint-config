2fb049bdeecb2996e36cbe1d457a0ba754e2b5a2
vsepchat <vsepchat@20minutes.fr>
2017-08-17 16:34:08 +0200

    init 20Minutes eslint config

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+  },
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "plugins": [
+    "babel"
+  ],
+  "rules": {
+    "semi": [2, "never"],
+    "no-console": [1, {
+      "allow": ["warn", "error"]
+    }],
+    "curly": [1, "all"],
+    "no-param-reassign": [1, {
+      "props": false
+    }],
+    "import/prefer-default-export": 0,
+    "no-multiple-empty-lines": [1, { "max": 2, "maxEOF": 1, "maxBOF": 0 }],
+    "padding-line-between-statements": [
+        "error",
+        { "blankLine": "always", "prev": "*", "next": "return" },
+        { "blankLine": "always", "prev": "import", "next": "*" },
+        { "blankLine": "never", "prev": "import", "next": "import" },
+    ]
+  }
+}
